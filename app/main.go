3b474827e3c91ce4abe30047dc25614dd7c87b50
hemanta212 <sharmahemanta.212@gmail.com>
2022-11-17 10:43:43 +0545

    Get number of cells of a table

--- app/main.go
+++ app/main.go
@@ -5,27 +5,62 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 	// Available if you need it!
 	// "github.com/pingcap/parser"
 	// "github.com/pingcap/parser/ast"
 )
-
-type SQLiteSchemaRow struct {
-	_type    string // _type since type is a reserved keyword
-	name     string
-	tblName  string
-	rootPage int
-	sql      string
-}
 
 // Usage: your_sqlite3.sh sample.db .dbinfo
 func main() {
 	databaseFilePath := os.Args[1]
 	command := os.Args[2]
 
+	if len(command) == 0 {
+		fmt.Println("Usage: your_sqlite3.sh sample.db .dbinfo")
+		return
+	}
+	if command[0] == '.' {
+		parseDotCommands(databaseFilePath, command)
+	} else {
+		parseSqlCommand(databaseFilePath, command)
+	}
+}
+
+func parseSqlCommand(databaseFilePath, command string) {
+	commandParts := strings.Split(command, " ")
+	tableName := commandParts[len(commandParts)-1]
+	fmt.Println("Got tableName: ", tableName)
+
+	databaseFile, err := os.Open(databaseFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, _ = databaseFile.Seek(100, io.SeekStart)
+	sqliteSchemaRows, err := parseSqliteSchemaRows(databaseFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pageSize := int64(getPageSize(databaseFile))
+	rootPage := -1
+	for _, row := range sqliteSchemaRows {
+		if row.tblName == tableName {
+			rootPage = row.rootPage
+		}
+	}
+	seekAt := (rootPage - 1) * int(pageSize)
+
+	_, _ = databaseFile.Seek(int64(seekAt), io.SeekStart) // Skip the database header
+	pageHeader := parsePageHeader(databaseFile)
+	fmt.Println(pageHeader.NumberOfCells)
+}
+
+func parseDotCommands(databaseFilePath, command string) {
 	switch command {
 	case ".dbinfo":
-		sqliteSchemaRows, err := parseSqliteSchemaRows(databaseFilePath)
+		sqliteSchemaRows, err := parseMainPage(databaseFilePath)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -33,7 +68,7 @@ func main() {
 		fmt.Printf("number of tables: %v", len(sqliteSchemaRows))
 
 	case ".tables":
-		sqliteSchemaRows, err := parseSqliteSchemaRows(databaseFilePath)
+		sqliteSchemaRows, err := parseMainPage(databaseFilePath)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -44,51 +79,8 @@ func main() {
 			}
 			fmt.Print(string(row.tblName), " ")
 		}
-
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
 	}
-}
-
-func parseSqliteSchemaRows(databaseFilePath string) ([]SQLiteSchemaRow, error) {
-	databaseFile, err := os.Open(databaseFilePath)
-	if err != nil {
-		return nil, err
-	}
-
-	headerString := parseString(databaseFile, 16)
-	fmt.Println(headerString)
-
-	_, _ = databaseFile.Seek(96, io.SeekStart) // Read the last header info about sqlite version number
-	sqlite3VersionNumber := parseUInt32(databaseFile)
-	fmt.Println("Sqlite3 Version Number: ", sqlite3VersionNumber)
-
-	_, _ = databaseFile.Seek(100, io.SeekStart) // Skip the database header
-
-	pageHeader := parsePageHeader(databaseFile)
-
-	cellPointers := make([]uint16, pageHeader.NumberOfCells)
-
-	for i := 0; i < int(pageHeader.NumberOfCells); i++ {
-		cellPointers[i] = parseUInt16(databaseFile)
-	}
-
-	var sqliteSchemaRows []SQLiteSchemaRow
-
-	for _, cellPointer := range cellPointers {
-		_, _ = databaseFile.Seek(int64(cellPointer), io.SeekStart)
-		parseVarint(databaseFile) // number of bytes in payload
-		parseVarint(databaseFile) // rowid
-		record := parseRecord(databaseFile, 5)
-
-		sqliteSchemaRows = append(sqliteSchemaRows, SQLiteSchemaRow{
-			_type:    string(record.values[0].([]byte)),
-			name:     string(record.values[1].([]byte)),
-			tblName:  string(record.values[2].([]byte)),
-			rootPage: int(record.values[3].(uint8)),
-			sql:      string(record.values[4].([]byte)),
-		})
-	}
-	return sqliteSchemaRows, nil
 }
